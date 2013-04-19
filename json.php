60fa28b1eb010ed9092fc3f36991ce4fe0da4ff9
Yuta Hayashi (skillpartners) <hayashi@skillpartners.co.jp>
2013-04-19 15:16:31 +0900

    修正

--- json.php
+++ json.php
@@ -1,50 +1,59 @@
 <?php
-	include_once("./resources/php/dBug.php");
-	header("Content-Type: text/html; charset=UTF-8");
-	//header("Content-Type: text/javascript; charset=utf-8");
-	//header("Content-Type: application/json; charset=utf-8");
-	
-	//h htmlspecialchars
+	include_once('./resources/php/dBug.php');
+//	header('Content-Type: text/html; charset=UTF-8');
+	header('Content-Type: application/json; charset=utf-8');
+
+	// h htmlspecialchars
 	$get = h($_GET);
-	
 	$url = $get['url'];
-	
-	$url = str_replace([".", "/"], "", $url) . ".json";
-	
-	$json = file_get_contents($url,true);
-	$data = json_decode($json);
-	
-	if($url == 'mockdata1.json'){
-		$output = $data -> {'id'} -> {$get['id']};
-	} else if($url == 'mockdata2.json'){
-		$output = $data -> {'no'} -> {$get['no']};
-	} else if($url == 'mockdata3.json'){
-		$id_arr = array();
-		$params = explode("&", $_SERVER['QUERY_STRING']);
-		foreach ($params as $param) {
-			$par = explode("=", $param);
-			if ($par[0] == "id") array_push($id_arr, $par[1]);
-		}
-		
-		for ($i = 0; $i < count($id_arr); $i++) {
-			$output[$i] = $data -> {'results'}[$id_arr[$i]];
-		}
-	} else {
-		$output = $data;
+	$url = str_replace(['../', '/'], '', $url);
+
+	// 拡張子がjsonかチェック
+	$ext = end(explode('.', $url));
+	if ($ext !== 'json') {
+	echo 'not json file...';
+	exit();
 	}
-	
-	$data -> {'results'} = $output;
-	
-	//new dBug($data);
-	echo json_encode($data);
-	
-	
-	//htmlspecialchars
+
+	$json = file_get_contents($url,true);
+	echo $json;
+
+
+//	$url = str_replace([".", "/"], "", $url) . ".json";
+//
+//	$json = file_get_contents($url,true);
+//	$data = json_decode($json);
+//
+//	if($url == 'mockdata1.json'){
+//		$output = $data -> {'id'} -> {$get['id']};
+//	} else if($url == 'mockdata2.json'){
+//		$output = $data -> {'no'} -> {$get['no']};
+//	} else if($url == 'mockdata3.json'){
+//		$id_arr = array();
+//		$params = explode("&", $_SERVER['QUERY_STRING']);
+//		foreach ($params as $param) {
+//			$par = explode("=", $param);
+//			if ($par[0] == "id") array_push($id_arr, $par[1]);
+//		}
+//
+//		for ($i = 0; $i < count($id_arr); $i++) {
+//			$output[$i] = $data -> {'results'}[$id_arr[$i]];
+//		}
+//	} else {
+//		$output = $data;
+//		$data -> {'results'} = $output;
+//
+//		//new dBug($data);
+//		echo json_encode($data);
+//	}
+
+
+		//htmlspecialchars
 	function h($str){
-		if(is_array($str)){
-			return array_map("h",$str);
-		}else{
-			return htmlspecialchars($str,ENT_QUOTES);
-		}
+	if(is_array($str)){
+		return array_map('h',$str);
+	}else{
+		return htmlspecialchars($str,ENT_QUOTES);
+	}
 	}
 ?>
--- main.js
+++ main.js
@@ -1,8 +1,27 @@
 /*!
  * main.js
  */
-;(function ($, window, MY, undefined) {
+;(function ($, window, undefined) {
 	$(document).ready( function () {
-		//action
+		// get url param
+		var vars = [], hash;
+		var hashes = window.location.href.slice(window.location.href.indexOf('?') + 1).split('&');
+		for(var i = 0; i < hashes.length; i++)
+		{
+			hash = hashes[i].split('=');
+			vars.push(hash[0]);
+			vars[hash[0]] = hash[1];
+		}
+
+		var url = vars.url;
+		$.ajax({
+			type: 'GET'
+			,url: 'json.php?url=' + url
+			,dataType:'json'
+			,success:function (res) {
+				console.log( res );
+			}
+		});
+		
 	});
-})(jQuery, this, jsonResponseAPI);
+})(jQuery, this);
